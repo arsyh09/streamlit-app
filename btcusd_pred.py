4024551fe8083e2e90b92ca0fcb798445900eb0f
Ahmad Reza Syahbana <60778430+arsyh09@users.noreply.github.com>
2023-10-29 19:35:33 +0800

    Update btcusd_pred.py

--- btcusd_pred.py
+++ btcusd_pred.py
@@ -11,8 +11,9 @@ import plotly.express as px
 import os
 
 # Mendapatkan nilai dari variabel lingkungan
-api_key = os.environ.get('API_KEY')
-api_secret = os.environ.get('API_SECRET')
+api_key = st.secrets.API.API_KEY
+api_secret = st.secrets.API.API_SECRET
+
 
 # Fungsi untuk mengambil data dari Binance API sesuai timeframe
 def get_binance_data(api_key, api_secret, selected_interval):
